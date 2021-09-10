30e1b58d60aac13a2594f8d703e4adf5a1247ef5
Foad Mohammadi <foad.mohammadi.fm@gmail.com>
2021-09-10 10:19:26 +0200

    Add openapi schema and docs in swagger ui and redoc

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Blog API',
+        default_version='v1',
+        description='A simple API for learning DRF',
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(email='hello@example.com'),
+        license=openapi.License(name='BSD License'),
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +23,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
