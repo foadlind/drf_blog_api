2aedbfa0ae212249e4f9454cfe84b12d5d317425
Foad Mohammadi <foad.mohammadi.fm@gmail.com>
2021-09-10 09:46:30 +0200

    Add a user endpoint and refactor to viewset/router for views and urls

--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,17 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from django.contrib.auth import get_user_model
+from rest_framework.viewsets import ModelViewSet
 
 from posts.models import Post
 from posts.permissions import IsAuthorOrReadOnly
-from posts.serializers import PostSerializer
+from posts.serializers import PostSerializer, UserSerializer
 
 
-class PostList(ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class PostDetail(RetrieveUpdateDestroyAPIView):
+class PostViewSet(ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
